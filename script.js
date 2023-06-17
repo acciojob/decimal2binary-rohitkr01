7f736368117d5e1bd213c87aa0f16849f2f79f3c
Rohit Kumar <97820587+rohitkr01@users.noreply.github.com>
2023-06-17 17:25:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-function threeSum(arr, target) {
+function decimalToBinary(num) {
 // write your code here
-  
+	const binaryNumber = decimalNumber.toString(2);  
+	console.log(binaryNumber);
 }
 
-module.exports = threeSum;
+module.exports = decimalToBinary;
